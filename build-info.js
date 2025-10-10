104e77f3576a394b5f9901b3f8295b41ab6db69d
davidcarma <davidcarma@users.noreply.github.com>
2025-10-11 01:52:09 +0200

    Update build-info [build-info update]

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 // Auto-generated build information
 // This file is updated automatically on each commit
 window.BUILD_INFO = {
-    hash: 'dde8dd6',
-    hashFull: 'dde8dd69e9514af45e30c0a7b916695842df5f19',
-    date: '2025-10-11 01:52:09',
+    hash: 'a0ac94b',
+    hashFull: 'a0ac94b79a0915d7155e6ad7eb0cf36d39f8564e',
+    date: '2025-10-11 01:52:10',
     version: '1.0.0'
 };
