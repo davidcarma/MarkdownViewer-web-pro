a722fa5aa016b6258966c01fd0aada13338c8db9
David Saliba <david.saliba@gmail.com>
2025-10-11 02:08:50 +0200

    Update build-info [build-info update]

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 // Auto-generated build information
 // This file is updated automatically on each commit
 window.BUILD_INFO = {
-    hash: '19cdfec',
-    hashFull: '19cdfecd0b7c54126fff4055167be6528c596009',
+    hash: 'ecea1f5',
+    hashFull: 'ecea1f560558f8d91114f1a46efc99d5f4b1420d',
     date: '2025-10-11 02:08:50',
     version: '1.0.0'
 };
