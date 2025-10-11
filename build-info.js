b09b709bdcc5a9a29184a6812c4bd03d9ba711f5
David Saliba <david.saliba@gmail.com>
2025-10-11 02:10:53 +0200

    Add detailed debugging for Mermaid extraction

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 // Auto-generated build information
 // This file is updated automatically on each commit
 window.BUILD_INFO = {
-    hash: 'ecea1f5',
-    hashFull: 'ecea1f560558f8d91114f1a46efc99d5f4b1420d',
-    date: '2025-10-11 02:08:50',
+    hash: 'a722fa5',
+    hashFull: 'a722fa5aa016b6258966c01fd0aada13338c8db9',
+    date: '2025-10-11 02:10:53',
     version: '1.0.0'
 };
