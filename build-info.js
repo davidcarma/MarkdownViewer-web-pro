0f3e6d56ea41df6ed46ada588e97761c817da5a5
davidcarma <davidcarma@users.noreply.github.com>
2026-03-07 08:59:55 +0100

    Build: update deployed hash 595cc7f

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 // Auto-generated build information
 // This file is updated automatically on each commit
 window.BUILD_INFO = {
-    hash: '0b4f1a7',
-    hashFull: '0b4f1a7579f04352407b505d99d7217f8778a73b',
-    date: '2026-03-07 08:54:00',
+    hash: '595cc7f',
+    hashFull: '595cc7f63f473274af28d5f15428d00a662af2eb',
+    date: '2026-03-07 08:59:55',
     version: '1.0.0'
 };
