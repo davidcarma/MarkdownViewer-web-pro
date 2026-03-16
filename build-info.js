01b9b61cab99b840f72fecf239b333bb942e5dfc
davidcarma <davidcarma@users.noreply.github.com>
2026-03-16 22:53:19 +0100

    Build: update deployed hash d94ba67

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 // Auto-generated build information
 // This file is updated automatically on deploy push
 window.BUILD_INFO = {
-    hash: '5501c18',
-    hashFull: '5501c184de33611d991fcd411cc6a43ba29d6db5',
-    date: '2026-03-07 09:32:18',
+    hash: 'd94ba67',
+    hashFull: 'd94ba67b304244bbc6f2a60f6c3a83a478b222ee',
+    date: '2026-03-16 22:53:19',
     version: '1.0.0'
 };
